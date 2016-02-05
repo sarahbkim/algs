54396d58517ca46acd881364034bb7211b22791d
sarahbkim <pnkdaegee@gmail.com>
2016-02-05 09:56:22 -0800

    Percolation Stats

--- PercolationStats.java
+++ PercolationStats.java
@@ -7,40 +7,32 @@ import com.sarahkim.Percolation;
  * Created by sarahbkim on 2/5/16.
  */
 public class PercolationStats {
-    int gridSize;
-    int repeat;
     double[] thresholds;
-    double criticalValue = -1.96;
+    double criticalValue = -1.96; // z-value for 95% confidence interval
 
     // perform T independent experiments on an NxN grid
     public PercolationStats(int N, int T) {
-        gridSize = N;
-        repeat = T;
         thresholds = new double[T];
+        performAllExperiments(N, T);
     }
-
     private void performAllExperiments(int N, int reps) {
-        for(int i=1; i<reps;i++) {
-            performSingleExperiment(N);
+        for(int i=0; i<reps;i++) {
+            performSingleExperiment(N, i);
         }
     }
-
-    private void performSingleExperiment(int N) {
+    private void performSingleExperiment(int N, int round) {
         Percolation p = new Percolation(N);
         int opens = 0;
-        int rounds = 0;
 
         while(!p.percolates()) {
-            int randomI = StdRandom.uniform(N*N+1);
-            int randomJ = StdRandom.uniform(N*N+1);
-            System.out.println("Choosing random number " + randomI + ", " + randomJ);
+            int randomI = StdRandom.uniform(N) + 1;
+            int randomJ = StdRandom.uniform(N) + 1;
             p.open(randomI, randomJ);
             opens++;
             if(p.percolates()) {
-                thresholds[rounds] = (double)opens*1.0/N*N;
+                thresholds[round] = (double)opens/(N*N);
                 break;
             }
-            rounds++;
         }
     }
 
@@ -48,7 +40,6 @@ public class PercolationStats {
     public double mean() {
         return StdStats.mean(thresholds);
     }
-
     // sample std deviation of percolation threshold
     public double stddev() {
         return StdStats.stddev(thresholds);
@@ -63,11 +54,16 @@ public class PercolationStats {
     }
 
     private double marginOfError() {
-        return criticalValue * stddev()/Math.sqrt(gridSize*gridSize);
+        return criticalValue * stddev();
     }
 
     public static void main(String[] args) {
-
+        if(args.length < 2) {
+            throw new Error("Requires N (size of grid) and T(number of experiements");
+        }
+        PercolationStats pStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        System.out.println("mean = " + pStats.mean());
+        System.out.println("stddev = " + pStats.stddev());
+        System.out.println("95% confidence interval = " + pStats.confidenceLo() + ", " + pStats.confidenceHi());
     }
-
 }
