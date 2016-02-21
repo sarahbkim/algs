6dfe21e63d3faea66aa71e15ce1fa7827432cdc7
sarahbkim <pnkdaegee@gmail.com>
2016-02-20 18:04:13 -0800

    add tests and implement BruteCollinearPoints class

--- BruteCollinearPoints.java
+++ BruteCollinearPoints.java
@@ -1,17 +1,11 @@
 package com.sarahkim;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by sarahbkim on 2/19/16.
  */
-// Write a program BruteCollinearPoints.java that examines 4 points at a time and checks
-// whether they all lie on the same line segment, returning all such line segments.
-// To check whether the 4 points p, q, r, and s are collinear, check whether the three slopes between p and q,
-// between p and r, and between p and s are all equal.
 
 // Corner cases. Throw a java.lang.NullPointerException either the argument to the constructor is null
 // or if any point in the array is null. Throw a java.lang.IllegalArgumentException if the argument to
@@ -19,18 +13,31 @@ import java.util.Map;
 public class BruteCollinearPoints {
     int segmentCount;
     LineSegment[] arrayOfSegments;
-    HashMap<Double, ArrayList> slopes = new HashMap<Double, ArrayList>();
+    HashMap<Double, ArrayList<Point>> slopes = new HashMap<Double, ArrayList<Point>>();
 
-    public BruteCollinearPoints(Point[] points) {
-        // initialize segmentCount
+    public BruteCollinearPoints(Point[] points) throws Exception {
+        if(points == null) { throw new NullPointerException(); };
         segmentCount = 0;
+
+        validatePoints(points);
         calculateSlopes(points);
         calculateSegments();
+    }
+    private void validatePoints(Point[] points) throws Exception{
+        Arrays.sort(points);
+        for(int i=0;i<points.length;i++) {
+            if(points[i] == null) { throw new NullPointerException(); };
+            if(i < points.length-1) {
+                if (points[i].compareTo(points[i+1]) == 0) {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
     }
     private void calculateSlopes(Point[] points) {
         // calculate the slope
         for(int i=0;i<points.length;i++) {
-            for(int j=i+1;j<points.length-1;j++) {
+            for(int j=i+1;j<points.length;j++) {
                 double s = points[i].slopeTo(points[j]);
                 ArrayList list;
                 list = slopes.containsKey(s) ? slopes.get(s) : new ArrayList<Point>();
@@ -41,12 +48,29 @@ public class BruteCollinearPoints {
         }
     }
     private void calculateSegments() {
-        // loop through slopes
-        // if slope.value.length == 4,
-            // find lowest & highest point (sort by )
-            // create segment & save to arrayOfSegments
-            // increment segmentCount
+        ArrayList<LineSegment> tempSegments = new ArrayList<LineSegment>();
+        ArrayList<Double> keys = new ArrayList<Double>(slopes.keySet());
+        if(keys.size() > 0) {
+            for (int i = 0; i < keys.size(); i++) {
+                ArrayList<Point> value = slopes.get(keys.get(i));
+                if ((value.size() >= 4)) {
+                    tempSegments.add(createLineSegment(value));
+                }
+            }
+        }
+        arrayOfSegments = tempSegments.toArray((new LineSegment[tempSegments.size()]));
+        segmentCount = arrayOfSegments.length;
     }
+
+    private LineSegment createLineSegment(ArrayList<Point> value) {
+        // if slope.value.length == 4,
+        Point[] points = value.toArray(new Point[value.size()]);
+        Arrays.sort(points);
+        // create segment & save to arrayOfSegments
+        LineSegment s = new LineSegment(points[0], points[points.length-1]);
+        return s;
+    }
+
     public int numberOfSegments() {
         return segmentCount;
     }
--- BruteCollinearPointsTest.java
+++ BruteCollinearPointsTest.java
@@ -2,31 +2,59 @@ package com.sarahkim;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
-import java.util.Random;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.lang.reflect.Method;
 
 /**
  * Created by sarahbkim on 2/19/16.
  */
 public class BruteCollinearPointsTest {
     private BruteCollinearPoints b;
-    private Point[] points = new Point[5];
-
+    private BruteCollinearPoints b2;
+    private Point[] points = new Point[4];
+    private Point[] points2 = new Point[4];
     @Before
     public void setUp() throws Exception {
-        int i=0;
-        Random ran = new Random();
+        // fill in arr
+        points[0] = (new Point(0, 0));
+        points[1] = (new Point(-1, 3));
+        points[2] = (new Point(5, 5));
+        points[3] = (new Point(-10, 3));
 
-        while(i<5) {
-            int x = (ran.nextInt(65536)-32768);
-            int y = (ran.nextInt(65536)-32768);
-            points[i] = new Point(x, y);
-            i++;
-        }
+        // fill in arr2
+        points2[0] = (new Point(0, 0));
+        points2[1] = (new Point(1, 1));
+        points2[2] = (new Point(2, 2));
+        points2[3] = (new Point(3, 3));
+
         b = new BruteCollinearPoints(points);
+        b2 = new BruteCollinearPoints(points2);
     }
     @Test
-    public void initTest() throws Exception {
-        System.out.println(b.slopes);
+    public void testInitB1() throws Exception {
+        assertEquals(0, b.numberOfSegments());
+        assertEquals(0, b.segments().length);
+    }
+    @Test
+    public void testInitB2() throws Exception {
+        assertEquals(1, b2.numberOfSegments());
+        assertEquals(1, b2.segments().length);
+        assertEquals("(0, 0) -> (3, 3)", b2.segments()[0].toString());
+    }
+    @Test(expected = NullPointerException.class)
+    public void testNullPoints() throws Exception {
+        BruteCollinearPoints t = new BruteCollinearPoints(null);
+    }
+    @Test(expected = NullPointerException.class)
+    public void testnullPoint() throws Exception {
+        Point[] pointsWithNull = new Point[]{new Point(0, 0), null, new Point(3, 3)};
+        BruteCollinearPoints t = new BruteCollinearPoints(pointsWithNull);
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testRepeatedPoint() throws Exception {
+        Point[] pointsWithRep = new Point[]{new Point(0, 0), new Point(-30, 3), new Point(3, 3), new Point(0, 0), new Point(3, 3)};
+        BruteCollinearPoints t = new BruteCollinearPoints(pointsWithRep);
     }
 }
