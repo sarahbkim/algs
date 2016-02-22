2cf7a3dceceaa8374d0d6cae8bcb3e0c251dad72
sarahbkim <pnkdaegee@gmail.com>
2016-02-22 09:04:46 -0800

    get rid of duplicate line segments

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -1,68 +1,122 @@
 package com.sarahkim;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Stream;
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 
-/**
- * Created by sarahbkim on 2/20/16.
- */
+import javax.sound.sampled.Line;
+
 public class FastCollinearPoints {
     private LineSegment[] segments;
+    private ArrayList<LineSegment> tempSegments;
 
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
+        tempSegments = new ArrayList<LineSegment>();
         if (points == null) {
             throw new NullPointerException();
         }
         Arrays.sort(points);
-        Point origin = points[0];
-        calculateSlope(origin, points);
-    }
-    private void calculateSlope(Point origin, Point[] points) {
-        double currSlope = origin.slopeTo(points[0]);
-        int sameCount = 0;
-        int start = 0;
-        ArrayList<LineSegment> tempSegments = new ArrayList<LineSegment>();
+        for (int i=0; i<points.length;i++) {
+            Point origin = points[i];
+            // remove origin from array
+            Point[] firstHalf = Arrays.copyOfRange(points, 0, i);
+            Point[] secondHalf = i + 1 < points.length ? Arrays.copyOfRange(points, i + 1, points.length) : null;
 
+            // join new sub arrays
+            Stream<Point> sa = Arrays.stream(firstHalf);
+            Stream<Point> sb = secondHalf != null ? Arrays.stream(secondHalf) : null;
+            Point[] toSort = sb != null ? Stream.concat(sa, sb).toArray(size -> new Point[size]) : firstHalf;
+
+            // sort by slope
+            Point[] sorted = sortedBySlope(toSort, origin);
+            Point[] test = sortedBySlope(points, origin);
+
+            // find collinear points
+            getCollinearPoints(test, origin);
+        }
+        convertTempSegments();
+    }
+    private Point[] sortedBySlope(Point[] arr, Point origin) {
         // Sort by slope
-        Arrays.sort(points, origin.slopeOrder());
+        Arrays.sort(arr, origin.slopeOrder());
+        return arr;
+    }
+    private void getCollinearPoints(Point[] sortedPoints, Point origin) {
+        int hasSameSlope = 0;
+        int start = 0;
 
         // Find equal slopes & create line segments
-        for(int i=1;i<points.length-1;i++) {
+        for(int i=0;i<sortedPoints.length;i++) {
             // Validate Points
-            if ( i < points.length-2 ) {
-                validatePoint(origin, points[i], points[i + 1]);
-            }
-            double nextSlope = origin.slopeTo(points[i]);
-            if (currSlope == nextSlope) {
-                sameCount++;
-            }
-            if (currSlope != nextSlope) {
-                currSlope = nextSlope;
-                if (sameCount >= 3)  {
-                    // create line segment from start to end
-                    LineSegment l = new LineSegment(points[start], points[i-1]);
-                    tempSegments.add(l);
+            Point next = i+1 < sortedPoints.length ? sortedPoints[i+1] : null;
+            Point curr = sortedPoints[i];
+            validatePoint(origin, curr, next);
+
+            // compare slopes curr/next
+            if (next != null) {
+                double currSlope = origin.slopeTo(curr);
+                double nextSlope = origin.slopeTo(next);
+                if (currSlope == nextSlope) {
+                    hasSameSlope++;
+                } else {
+                    if (hasSameSlope >= 2) {
+                        ArrayList<Point> segmentPoints = new ArrayList<Point>();
+                        segmentPoints.add(origin);
+                        int k = start;
+                        while(k <= i) {
+                            segmentPoints.add(sortedPoints[k]);
+                            k++;
+                        }
+                        Point[] s = segmentPoints.toArray((new Point[segmentPoints.size()]));
+                        Arrays.sort(s);
+                        createLineSegment(s);
+                    }
+                    hasSameSlope = 0; // reset
+                    start = i + 1;
                 }
-                sameCount = 0;
-                start = i;
             }
         }
+    }
+    private void convertTempSegments() {
         segments = tempSegments.toArray((new LineSegment[tempSegments.size()]));
+    }
+    private String pointsArrtoString(Point[] points) {
+        String s = "";
+        for(int i=0;i<points.length;i++) {
+            s += points[i].toString() + ", ";
+        }
+        return s;
+    }
+    private void createLineSegment(Point[] points) {
+        Point min = points[0];
+        Point max = points[points.length - 1];
+        LineSegment newL = new LineSegment(min, max);
+        if (tempSegments.size() == 0) {
+            tempSegments.add(newL);
+        } else {
+            for (int i = 0; i < tempSegments.size(); i++) {
+                LineSegment l = tempSegments.get(i);
+                if (l.toString().equals(newL.toString())) {
+                    return;
+                }
+            }
+            tempSegments.add(newL);
+        }
     }
     private void validatePoint(Point origin, Point p, Point next) {
         if (p == null || origin == null) {
             throw new NullPointerException();
         }
-        if (origin.compareTo(p) == 0) {
+//        if (origin.compareTo(p) == 0) {
+//            throw new IllegalArgumentException();
+//        }
+        if (next != null && p.compareTo(next) == 0) {
             throw new IllegalArgumentException();
-        }
-        if (next != null) {
-            if (p.compareTo(next) == 0) {
-                throw new IllegalArgumentException();
-            }
         }
 
     }
