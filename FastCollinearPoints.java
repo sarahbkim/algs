c208a9e7f171ff2b7da5b2820b6a495138cdc7fd
sarahbkim <pnkdaegee@gmail.com>
2016-02-21 21:26:18 -0800

    add main method for testing

--- FastCollinearPoints.java
+++ FastCollinearPoints.java
@@ -1,8 +1,9 @@
 package com.sarahkim;
-import javax.sound.sampled.Line;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
 /**
  * Created by sarahbkim on 2/20/16.
@@ -70,5 +71,33 @@ public class FastCollinearPoints {
     }
     public LineSegment[] segments() {
         return segments;
+    }
+    public static void main(String[] args) {
+
+        // read the N points from a file
+        In in = new In(args[0]);
+        int N = in.readInt();
+        Point[] points = new Point[N];
+        for (int i = 0; i < N; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.show(0);
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        FastCollinearPoints collinear = new FastCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
     }
 }
